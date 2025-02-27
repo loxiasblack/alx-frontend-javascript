60761e26050a67c415d29f8adf2024d78f9f2e38
loxiasblack <iliaschokairi@gmail.com>
2025-02-27 22:02:44 +0000

    arrow

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-const returnHowManyArguments = (...args) => args.length;
-
-export default returnHowManyArguments;
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
