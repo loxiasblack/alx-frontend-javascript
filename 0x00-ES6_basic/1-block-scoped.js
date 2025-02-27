8acca7429c56b511ee3bb0dd9969da34b06309d0
loxiasblack <iliaschokairi@gmail.com>
2025-02-27 20:09:04 +0000

    block scoped fix hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
-
-  
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
+  }
   return [task, task2];
 }
